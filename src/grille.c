32f869cf67af8fe5b7515270c2cd3b11b351a2d7
PrimFX <contact@fboris.com>
2018-10-16 19:27:11 +0200

    Fonction alloue_grille corrigee

--- src/grille.c
+++ src/grille.c
@@ -1,13 +1,12 @@
 #include "grille.h"
 
 void alloue_grille (int l, int c, grille* g){
-	//g = (grille) malloc(sizeof(grille));
 	int i, j;
 	g->nbl = l;
 	g->nbc = c;
 	g->cellules = (int**) malloc(l * sizeof(int*));
 
-	for (i = 0; i < c; i++) {
+	for (i = 0; i < l; i++) {
 		g->cellules[i] = (int*) malloc(c * sizeof(int));
 		for (j = 0; j < c; j++) {
 			set_morte(i, j, *g);
@@ -32,7 +31,7 @@ void init_grille_from_file (char * filename, grille* g){
 	alloue_grille(l,c,g);
 	
 	fscanf(pfile, "%d", & vivantes);
-	for (n=0; n< vivantes; ++n){
+	for (n=0; n < vivantes; ++n){
 		fscanf(pfile, "%d", & i);
 		fscanf(pfile, "%d", & j);
 		set_vivante(i,j,*g);
